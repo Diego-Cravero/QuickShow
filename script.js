c7032c0387c115de5029a868f41064128df00515
Cvave <Cvave@MSI.(none)>
2026-02-05 13:52:19 +0100

    fine modifiche

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-let domanda  = ["quanto fa 5 + 3?", "capitale italiana?","JS è un programma intpretato"]
+let domande = [
+  "quanto fa 5 + 3?",
+  "capitale italiana?",
+  "JS è un linguaggio?"
+];
 
-let risposte = [["6", "7" , "8" , "9 "],
-                ["Roma","napoli","Sanfre","Bra"],
-                ["Si","No","Forse","Chiedo a Teodolinda"]] 
+domande= [0,1,2]
+domande.sort() //ordinamento vettore 
 
 
-let _domanda = document.getElementById("Domanda")
-_domanda.textContent=domanda
+let risposte = [
+  ["8", "7", "6", "9"],
+  ["Roma", "Napoli", "Sanfre", "Bra"],
+  ["Si", "No", "Forse", "Chiedo a Teodolinda"]
+];
 
-let _risposte = document.getElementsByTagName("span")
+let indice = 0;
+let x
+let radios=document.getElementsByTagName("risposta")
+let soluzioni = [0,0,3]
+let _domanda = document.getElementById("Domanda");
+let _risposte = document.getElementsByTagName("span");
+radios = document.querySelector('input[type="radio"]') //prende il primo 
+/*radios = document.querySelectorAll("p") li prende tutti */
 
-for(let i=0 ; i<4;i++){
-    _risposte[i].textContent  = risposte[i]
+caricaDomanda()
+function caricaDomanda() {
+    
+    _domanda.textContent = domande[indice];
+    for (let j = 0; j < 4; j++) {
+        _risposte[j].textContent = risposte[indice][j];
+        //radios[i].checked = false chiedere 
+    }
+    
+}
+function confermaDomanda() {
+    let cont 
+    let scelta= -1
+    for(let i = 0;i<radios.length;i++){
+       if(radios[i].checked){
+            scelta=i
+       }
+    }
+    if(scelta == -1)
+    {
+        alert("seleziona una risposta")
+    }else
+    {
+        if(scelta==correte[indice]){
+            alert("RIsposta corretta")
+            cont++
+        }
+        else{
+            alert("Risposta errata")
+        }
+    }
+    indice++
+    if(indice<domande.length){
+        caricaDomanda()
+    }
+    else{
+        alert("fine Domande ")
+    }
+    
+        
+    
 }
